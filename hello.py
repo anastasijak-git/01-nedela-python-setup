79bade91696d9183dc62933f66f92a5e3e742abe
Anastasija Kraukle <nastjaa54@gmail.com>
2026-04-28 22:48:23 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+print('Hello from VS Code')
+git init
+git add .
+git commit -m "Initial commit - environment setup"
+git remote add origin <https://github.com/anastasijak-git/01-nedela-python-setup>
+git branch -M main
+git push -u origin main
